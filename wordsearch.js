49f77ed3ba2c339c55aba99053b2dc505caeda00
Sahil Athia <sahilathia918@gmail.com>
2020-11-04 22:06:47 +0000

    completed challange

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const reversed = word.split("").reverse().join("");
+    if (!letters || !word || letters.length <= 0) {
+      return false;
+    } else {
+      const horizontalJoin = letters.map((ls) => ls.join(""));
+      for (l of horizontalJoin) {
+        if (l.includes(word) || l.includes(reversed)) return true;
+      }
+      for (let i = 0; i < letters[0].length; i++) {
+        let verticalJoin = letters.map((x) => x[i]);
+        verticalJoin = verticalJoin.join("");
+        if (verticalJoin.includes(word) || verticalJoin.includes(reversed)) {
+          return true;
+        }
+      }
+      return false;
     }
-}
-
-module.exports = wordSearch
+  };
+  
+  module.exports = wordSearch;
+  
